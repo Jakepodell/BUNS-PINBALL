7db3db828ece9ac3ae5e8e6219e93ac13f4e7baa
019304 <019304@gmail.com>
2015-05-20 12:43:23 -0400

    First Day of Ball design

--- Ball.java
+++ Ball.java
@@ -1,4 +1,32 @@
-public class Ball{
-  
-asdfsadfsadf
+import java.awt.geom;
+public class Ball extends Ellipse2D.Double
+{
+  private double directionAngle;
+  private int speed;
+  public static final int HEIGHT=25;
+  public static final int WIDTH=25;
+  public Ball(int px, int py)
+  {
+	  super(px, py, HEIGHT, WIDTH);
+  }
+
+  public boolean intersects(Shape pshape)
+  {
+	  Area ballArea = new Area(this);
+	  Area objectArea = new Area(pshape);
+
+	  if(ballArea.intersects(objectArea))
+	  	return true;
+	  else
+	  	return false;
+  }
+
+  public int getDirectionAngle()
+  {
+    return directionAngle;
+  }
+  public int getSpeed()
+  {
+    return speed;
+  }
 }
