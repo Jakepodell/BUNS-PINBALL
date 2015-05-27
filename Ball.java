3a0876024ad4b402252d293a5fa17c9ed027abbb
Mokhir <dbalagyozyan@gmail.com>
2015-05-27 12:24:59 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,37 +1,29 @@
 import java.awt.geom.*;
 import java.awt.Shape;
 import java.awt.*;
-public class Ball extends Ellipse2D.Double
-{
+public class Ball extends Ellipse2D.Double{
   private double directionAngle;
   private int speed;
   public static final int HEIGHT=25;
   public static final int WIDTH=25;
   public static int currentXcoord;
   public static int currentYcoord;
-  public Ball(int px, int py)
-  {
+  public Ball(int px, int py){
 	  super(px,py, HEIGHT, WIDTH);
   	  currentXcoord = px;
 	  currentYcoord = py;
 
   }
-  public void drawBall(Graphics2D g2d)
-  {
-	  //super.paintComponent(g2d);
-	  //Ellipse2D.Double newCircle = new Ellipse2D.Double(currentXcoord, currentYcoord, HEIGHT, WIDTH);
+  public void drawBall(Graphics2D g2d){
 	  g2d.fill(this);
-
   }
   public void updateBall(int px, int py, Graphics2D g2d){
 	  currentXcoord = px;
 	  currentYcoord = py;
 	  drawBall(g2d);
-	  // graphics2d .translate(px,py)
   }
 
-  public boolean intersects(Shape pshape)
-  {
+  public boolean intersects(Shape pshape){
 	  Area ballArea = new Area(this);
 	  Area objectArea = new Area(pshape);
 	  Area intersectionArea = ballArea;
@@ -43,12 +35,10 @@ public class Ball extends Ellipse2D.Double
 	  	return true;
   }
 
-  public double getDirectionAngle()
-  {
+  public double getDirectionAngle(){
     return directionAngle;
   }
-  public int getSpeed()
-  {
+  public int getSpeed(){
     return speed;
   }
 }
