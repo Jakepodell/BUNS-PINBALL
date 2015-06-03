81f67e801120d2fda2fd30e72a0e0dd00bdb4319
Jake Podell <Jake@jakepodell.com>
2015-06-03 12:32:25 -0400

    Update Flipper.java

--- Flipper.java
+++ Flipper.java
@@ -3,13 +3,13 @@ import java.awt.event.*;
 import javax.swing.*;
 public class Flipper
 {
+	public static final int WIDTH=50;
+	public static final int HEIGHT=30;
 	boolean isUp = false;
-	int x2;
-	int y2;
 	int x1;
 	int y1;
 	int xlength = 50;
-	int ylength = 50;
+	int ylength = 30;
 	public Flipper(int x1, int y1)
 	{
 		this.x1 = x1;
@@ -20,10 +20,4 @@ public class Flipper
 		ylength*=-1;
 		isUp=!isUp;
 	}
-	public void drawFlip(Graphics2D g)
-	{
-
-		g.drawLine(x1,y1,(x1+xlength),(y1+ylength));
-	}
-
 }
