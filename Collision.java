ad186126145317e93afa22863ad03fd3f7e287df
Jake Podell <Jake@jakepodell.com>
2015-06-10 12:14:26 -0400

    Update Collision.java

--- Collision.java
+++ Collision.java
@@ -25,13 +25,53 @@ public class Collision{
       return false;
     }
     public static void checkBoardIntersection(Board board, Ball ball){
-		if(ball.getRightSide()>=board.getRightSide()
-		|| ball.getLeftSide()<=board.getLeftSide()){
+		System.out.println(ball.getRightSide());
+		if(ball.getRightSide()==board.getRightSide()
+		|| ball.getLeftSide()==board.getLeftSide()){
 			ball.flipXVelocity();
+			ball.moveHoriz();
 		}
-		if(ball.getBottomSide()>=board.getBottomSide()
-		|| ball.getTopSide()<=board.getTopSide()){
+		if(ball.getBottomSide()==board.getBottomSide()
+		|| ball.getTopSide()==board.getTopSide()){
 			ball.flipYVelocity();
+			ball.moveVert();
+			return;
+		}
+		checkBottomIntersection(board,ball);
+	}
+	public static void checkBottomIntersection(Board board, Ball ball){
+		if(ball.getY()+Ball.HEIGHT==Flipper.Y&&
+		(ball.getX()<LeftFlipper.X || ball.getX()>RightFlipper.X)){
+			ball.flipYVelocity();
+			ball.moveVert();
+		}
+	}
+	public static void checkBumperCollision(Bumper bumper, Ball ball){
+		double bumperCenterX = bumper.getX()+Bumper.WIDTH/2;
+		double bumperCenterY = bumper.getY()+Bumper.HEIGHT/2;
+		double ballCenterX = ball.getX()+Ball.WIDTH/2;
+		double ballCenterY = ball.getY()+Ball.HEIGHT/2;
+		double radicalTwo = Math.pow(2,.5);
+		double s = Bumper.WIDTH/2/(radicalTwo/1.05);
+
+		if(intersectsShape(ball,bumper)){
+			if(ball.getBottomSide()<=bumperCenterY-s || ball.getTopSide()>=bumperCenterY+s)
+				ball.flipYVelocity();
+			if(ball.getLeftSide()>=bumperCenterX+s || ball.getRightSide()<=bumperCenterX-s)
+				ball.flipXVelocity();
+			else{
+				ball.flipXVelocity();
+				ball.flipYVelocity();
+			}
+		}
+	}
+
+	public static void checkFlipperCollision(Flipper[] flips, Ball ball){
+		for(Flipper f: flips){
+			if(f.isUp()&&ball.getYVelocity()>0&&intersectsShape(ball,f.getCollisionArea())){
+				ball.flipYVelocity();
+				ball.addYVelocity();
+			}
 		}
 	}
 }
