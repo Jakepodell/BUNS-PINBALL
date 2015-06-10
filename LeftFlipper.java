71bc66c90e5b69f951bc4694df9d9b17fbcd2ad0
Jake Podell <Jake@jakepodell.com>
2015-06-10 12:13:40 -0400

    Update LeftFlipper.java

--- LeftFlipper.java
+++ LeftFlipper.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 public class LeftFlipper extends Flipper{
-
+	public static final int X = 150;
 	public LeftFlipper(int px, int py){
 		super(px,py);
+		int[] triangleXs = {X,X+Flipper.WIDTH,X+Flipper.WIDTH};
+		int[] triangleYs = {Flipper.Y, Flipper.Y-Flipper.HEIGHT,Flipper.Y+Flipper.HEIGHT};
+		triangleArea = new Polygon(triangleXs,triangleYs,3);
 	}
 
 	public void drawFlip(Graphics2D g)
