07ea31203cb2ded9e6f41fb029126e1ab5cce985
Jake Podell <Jake@jakepodell.com>
2015-06-10 12:12:14 -0400

    Update Flipper.java

--- Flipper.java
+++ Flipper.java
@@ -1,15 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.geom.*;
 public class Flipper
 {
 	public static final int WIDTH=50;
 	public static final int HEIGHT=30;
+	public static final int Y = 450;
 	boolean isUp = false;
 	int x1;
 	int y1;
 	int xlength = 50;
 	int ylength = 30;
+	Polygon triangleArea;
 	public Flipper(int x1, int y1)
 	{
 		this.x1 = x1;
@@ -20,4 +23,11 @@ public class Flipper
 		ylength*=-1;
 		isUp=!isUp;
 	}
+	public Area getCollisionArea(){
+		return new Area(triangleArea);
+	}
+	public boolean isUp(){
+		return isUp;
+	}
+
 }
