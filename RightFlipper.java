1ac4012b6f0b85027cab927548e6027764acb523
Jake Podell <Jake@jakepodell.com>
2015-06-10 12:13:56 -0400

    Update RightFlipper.java

--- RightFlipper.java
+++ RightFlipper.java
@@ -1,8 +1,12 @@
 import java.awt.*;
 public class RightFlipper extends Flipper{
 
+	public static final int X = 295;
 	public RightFlipper(int px, int py){
 		super(px,py);
+		int[] triangleXs = {X,X-Flipper.WIDTH,X-Flipper.WIDTH};
+		int[] triangleYs = {Flipper.Y, Flipper.Y-Flipper.HEIGHT,Flipper.Y+Flipper.HEIGHT};
+		triangleArea = new Polygon(triangleXs,triangleYs,3);
 	}
 
 	public void drawFlip(Graphics2D g)
