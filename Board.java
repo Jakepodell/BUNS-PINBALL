485faa123d6220e4b0e94e7da24e685f4d792ded
Jake Podell <Jake@jakepodell.com>
2015-06-11 12:04:48 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -5,17 +5,18 @@ import java.lang.Object;
 import java.awt.geom.RectangularShape;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
+import java.util.*;
 public class Board extends Rectangle2D.Double
 {
-	Ball[] balls = {(new Ball(300,0)),(new Ball(300,0)),(new Ball(300,0))};
+	Ball[] balls = {(new Ball(300,getBoardY())),(new Ball(300,getBoardY())),(new Ball(300,getBoardY()))};
+	ArrayList<Bumper> bumpers = new ArrayList<Bumper>();
 	int ballIndex=0;
 	Ball ball = balls[0];
-	Bumper bumper = new Bumper(200,200);
-	Bumper bumper2 = new Bumper(310,400);
 	public final static int X=0;
 	public final static int Y=0;
 	public final static int WIDTH=350;
 	public final static int HEIGHT=500;
+	//System.out.println("Fdsf");
 	LeftFlipper lf = new LeftFlipper(LeftFlipper.X,Flipper.Y);
 	RightFlipper rf = new RightFlipper(RightFlipper.X,Flipper.Y);
 	public Board(int xcord, int ycord, int height, int width)
@@ -27,6 +28,13 @@ public class Board extends Rectangle2D.Double
 		Y+PinballDriver.APPLETPADDING,
 		WIDTH,
 		HEIGHT);
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2-30),200));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2+30),200));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2),150));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2-135),300));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2-90),300));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2+135),300));
+		bumpers.add(new Bumper((int)(getX()+Board.WIDTH/2+90),300));
 	}
 	public void drawBoard(Graphics2D g)
 	{
@@ -36,8 +44,8 @@ public class Board extends Rectangle2D.Double
 		ball.drawBall(g);
 		lf.drawFlip(g);
 		rf.drawFlip(g);
-		bumper.drawBumper(g);
-		bumper2.drawBumper(g);
+		for(Bumper bumper:bumpers)
+			bumper.drawBumper(g);
 		//g.fill(lf.getCollisionArea());
 		//g.fill(rf.getCollisionArea());
 	}
@@ -45,16 +53,14 @@ public class Board extends Rectangle2D.Double
 		Flipper[] flips = {rf,lf};
 		for(int i =0; i<Math.abs(ball.getXVelocity()); i++){
 			Collision.checkBoardIntersection(this,ball);
-			Collision.checkBumperCollision(bumper,ball);
-			Collision.checkBumperCollision(bumper2,ball);
+			Collision.checkBumperCollision(bumpers,ball);
 			Collision.checkFlipperCollision(flips,ball);
 			ball.moveHoriz();
 
 		}
 		for(int i =0; i<Math.abs(ball.getYVelocity()); i++){
 			Collision.checkBoardIntersection(this,ball);
-			Collision.checkBumperCollision(bumper,ball);
-			Collision.checkBumperCollision(bumper2,ball);
+			Collision.checkBumperCollision(bumpers,ball);
 			Collision.checkFlipperCollision(flips,ball);
 			ball.moveVert();
 		}
